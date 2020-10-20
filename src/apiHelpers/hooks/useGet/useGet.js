b61c6003edb79297b37b7526cffabc833535879d
Remi Deprez <grolent@gmail.com>
2020-10-20 23:19:39 +0200

    fix useGet useCallback dependencies

--- src/apiHelpers/hooks/useGet/useGet.js
+++ src/apiHelpers/hooks/useGet/useGet.js
@@ -1,52 +1,47 @@
-import { get } from 'apiHelpers';
-import { useState, useCallback, useEffect } from 'react';
+import { get } from "apiHelpers";
+import { useState, useCallback, useEffect } from "react";
 
-const useGet = (url) => {
+const useGet = url => {
   const [result, setResult] = useState({
     data: null,
     error: null,
     status: null,
-    loading: true,
+    loading: true
   });
 
-  const memoizedGet = useCallback(
-    async () => {
-      if (!result.loading)
-        setResult((previousResult) => ({ ...previousResult, loading: true }));
-      const response = await get({
-        url,
-      });
+  const memoizedGet = useCallback(async () => {
+    setResult(previousResult => ({ ...previousResult, loading: true }));
+    const response = await get({
+      url
+    });
 
-      if (response.hasError) {
+    if (response.hasError) {
+      setResult({
+        data: null,
+        error: response.error,
+        status: response.status,
+        loading: false
+      });
+    } else {
+      try {
+        const data = await response.json();
+
+        setResult({
+          data,
+          error: null,
+          status: response.status,
+          loading: false
+        });
+      } catch (error) {
         setResult({
           data: null,
-          error: response.error,
+          error,
           status: response.status,
-          loading: false,
+          loading: false
         });
-      } else {
-        try {
-          const data = await response.json();
-
-          setResult({
-            data,
-            error: null,
-            status: response.status,
-            loading: false,
-          });
-        } catch (error) {
-          setResult({
-            data: null,
-            error,
-            status: response.status,
-            loading: false,
-          });
-        }
       }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [url]
-  );
+    }
+  }, [url]);
 
   useEffect(() => {
     memoizedGet();
